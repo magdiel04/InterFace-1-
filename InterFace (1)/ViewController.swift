abaf6c3d6f0d3251ea87e053de0a77fa7051365c
Magdiel <magdiel.altynbekov@gmail.com>
2022-11-07 18:47:53 +0600

    Initial Commit

---
+++ InterFace (1)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterFace (1)
+//
+//  Created by Magdiel Altynbekov on 7/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ InterFace (1)UITests/InterFace__1_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  InterFace__1_UITestsLaunchTests.swift
+//  InterFace (1)UITests
+//
+//  Created by Magdiel Altynbekov on 7/11/22.
+//
+
+import XCTest
+
+class InterFace__1_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
